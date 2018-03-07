d55fe311c95f479ea00c4a2f50177336e58550f1
Pierre ODIN <pierre.odin@hotmail.fr>
2018-03-07 19:58:08 +0100

    Add intro with image placeholder instead of animation

--- v2/src/blockfood.io/components/cloud/Cloud.js
+++ v2/src/blockfood.io/components/cloud/Cloud.js
@@ -1,14 +1,44 @@
 import React from 'react'
-import {cleanUrl} from '../../utils/Utils'
+import ReactDOM from 'react-dom'
+import {cleanUrl, getScrollValue} from '../../utils/Utils'
 
 import './Cloud.scss'
 
 import icon_Bf from '../../style/images/icon_Bf.svg'
 
-export default ({index}) => (
-    <div className="cloud">
-        <svg width="150" height="150">
-            <use xlinkHref={cleanUrl(icon_Bf + `#cloud_${index < 10 ? `0${index}` : index}`)}/>
-        </svg>
-    </div>
-)
+export default class Cloud extends React.Component {
+
+    constructor(props) {
+        super(props)
+
+        this.onScroll = this.onScroll.bind(this)
+    }
+
+    onScroll() {
+        this.parallaxElement.style.transform = `translateY(${this.props.depth * getScrollValue()}px)`
+    }
+
+    componentDidMount() {
+        this.parallaxElement = ReactDOM.findDOMNode(this).querySelector('.cloud-parallax')
+
+        this.props.depth && window.addEventListener('scroll', this.onScroll, false)
+    }
+
+    componentWillUnmount() {
+        this.props.depth && window.addEventListener('scroll', this.onScroll, false)
+    }
+
+    render() {
+        const {index, animated} = this.props
+
+        return (
+            <div className={`cloud${animated ? ` animated-${animated}` : ''}`}>
+                <div className='cloud-parallax'>
+                    <svg width="150" height="150">
+                        <use xlinkHref={cleanUrl(icon_Bf + `#cloud_${index < 10 ? `0${index}` : index}`)}/>
+                    </svg>
+                </div>
+            </div>
+        )
+    }
+}
--- v2/src/blockfood.io/pages/home/Home.js
+++ v2/src/blockfood.io/pages/home/Home.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import {cleanUrl} from '../../utils/Utils'
 
 import HeaderNavigationHandler from '../../components/header/handlers/HeaderNavigationHandler'
+import Cloud from '../../components/cloud/Cloud'
 
 import './Home.scss'
+
+import blockfoodIllustration from '../../style/images/blockfood-illustration.png'
+
 
 export default class Home extends React.Component {
     componentDidMount() {
@@ -16,11 +21,13 @@ export default class Home extends React.Component {
     render() {
         return (
             <div id="bfio-home">
-                <div>Intro</div>
-                <div id="about">About</div>
-                <div id="roadmap">Roadmap</div>
-                <div id="team">Team</div>
-                <div>More</div>
+                <section id="bfio-intro">
+                    <Cloud index="2" animated="1" depth="0.3"/>
+                    <Cloud index="7" animated="2" depth="0.05"/>
+                    <h1>BlockFood</h1>
+                    <h2>Food Delivery. Reinvented.</h2>
+                    <img src={cleanUrl(blockfoodIllustration)} alt="BlockFood Illustration"/>
+                </section>
             </div>
         )
     }
