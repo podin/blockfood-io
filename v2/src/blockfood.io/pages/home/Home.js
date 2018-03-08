43066dae58e6c0f19a91fb1cc6cce3f5d447e5e8
Pierre ODIN <pierre.odin@hotmail.fr>
2018-03-08 19:49:33 +0100

    Add animation02

--- v2/src/blockfood.io/pages/home/Home.js
+++ v2/src/blockfood.io/pages/home/Home.js
@@ -8,6 +8,7 @@ import Animation from '../../components/animation/Animation'
 import './Home.scss'
 
 import Animation01 from '../../style/animations/animation_01'
+import Animation02 from '../../style/animations/animation_02'
 import blockfoodIllustration from '../../style/images/blockfood-illustration.png'
 
 export default class Home extends React.Component {
@@ -23,7 +24,7 @@ export default class Home extends React.Component {
         return (
             <div id="bfio-home">
                 <section id="bfio-intro">
-                    <Cloud index="2" animated="1" depth="0.35"/>
+                    <Cloud index="2" animated="1" depth="0.28"/>
                     <Cloud index="7" animated="2" depth="0.01"/>
                     <h1>BlockFood</h1>
                     <h2>Food Delivery. Reinvented.</h2>
@@ -31,6 +32,15 @@ export default class Home extends React.Component {
                                placeholderAlt="BlockFood Illustration"
                                animation={Animation01}>
                         <Cloud index="4" animated="3"/>
+                    </Animation>
+                </section>
+                <section id="bfio-tutorial">
+                    <h3>How does it work?</h3>
+                    <p>The platform uses smart contracts to handle the payment and the progression of the orders placed by customers.</p>
+                    <Animation placeholderSrc={blockfoodIllustration}
+                               placeholderAlt="BlockFood Illustration"
+                               animation={Animation02}>
+                        <Cloud index="6" animated="2"/>
                     </Animation>
                 </section>
             </div>
